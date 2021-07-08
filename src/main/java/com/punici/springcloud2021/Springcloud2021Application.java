cba103561b27a1ad8a54f13e836843754c36f0ff
murloc <punici@163.com>
2021-07-08 22:22:08 +0800

    初始化

---
+++ src/main/java/com/punici/springcloud2021/Springcloud2021Application.java
@@ -0,0 +1,13 @@
+package com.punici.springcloud2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/punici/springcloud2021/Springcloud2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punici.springcloud2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloud2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
