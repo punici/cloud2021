cc2dabb4e27c51499ccf9f74143d5805f16c0e2d
murloc <punici@163.com>
2022-04-04 22:00:43 +0800

    model2

---
+++ provider-payment8001/src/main/java/com/punici/cloud/payment/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.punici.cloud.payment.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableOpenApi
+@Configuration
+public class SwaggerConfig
+{
+    // 是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
+    @Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled;
+    @Bean
+    public Docket createAdminApi()
+    {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                // 是否开启
+                .enable(swaggerEnabled).select()
+                // 扫描的路径包
+                .apis(RequestHandlerSelectors.basePackage("com.punici.cloud.payment.controller"))
+                // 指定路径处理PathSelectors.any()代表所有的路径
+                .paths(PathSelectors.any()).build().pathMapping("/");
+    }
+    
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfoBuilder().title("viomi-warehouse-web服务").description("中央库存swagger接口文档")
+                // 作者信息
+                .contact(new Contact("chenyusheng", "", "chenyusheng@viomi.com.cn")).version("1.0.0").build();
+    }
+}
